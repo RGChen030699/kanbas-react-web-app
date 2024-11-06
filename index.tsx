e1f12a6d6db319085057764ebf1fb1fffdfb891f
RGChen030699 <Chen.Rich@northeastern.edu>
2024-11-05 23:44:59 -0500

    Assignment 4 Redux

--- index.tsx
+++ index.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Lab1 from "./Lab1";
+import { Route, Routes, Navigate } from "react-router";
+import TOC from "./TOC";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Labs() {
+  return (
+  <Provider store={store}>
+   <div className="container-fluid">
+    <h1>Labs</h1>
+    <h2 style={{ fontWeight: "bold", fontSize: "24px" }}>Richard Chen</h2>
+    <TOC />
+    <Routes>
+     <Route path="Lab1" element={<Lab1 />} />
+     <Route path="Lab2" element={<Lab2 />} />
+     <Route path="Lab3" element={<Lab3 />} />
+     <Route path="Lab4" element={<Lab4 />} />
+    </Routes>
+   </div>
+  </Provider>
+ );} 
