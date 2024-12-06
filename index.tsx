41acc74c3e11d700edf54ca5a7461bc7437ec2a2
RGChen030699 <Chen.Rich@northeastern.edu>
2024-12-06 18:15:58 -0500

    a6

--- index.tsx
+++ index.tsx
@@ -1,28 +1,21 @@
-import Lab1 from "./Lab1";
-import Lab2 from "./Lab2";
-import Lab3 from "./Lab3";
-import Lab4 from "./Lab4";
-import Lab5 from "./Lab5";
-import { Route, Routes } from "react-router";
-import TOC from "./TOC";
-import store from "./store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-export default function Labs() {
-  return (
-    <Provider store={store}>
-      <div className="container-fluid">
-        <h1>Labs</h1>
-        <h2 style={{ fontWeight: "bold", fontSize: "24px" }}>Richard Chen</h2>
-        <TOC />
-        <Routes>
-          <Route path="Lab1" element={<Lab1 />} />
-          <Route path="Lab2" element={<Lab2 />} />
-          <Route path="Lab3" element={<Lab3 />} />
-          <Route path="Lab4" element={<Lab4 />} />
-          <Route path="Lab5" element={<Lab5 />} />
-        </Routes>
-      </div>
-    </Provider>
-  );
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
